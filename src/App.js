4918a0d5a744bb3a33af5c1903fb314744ccda3d
OLAYIIMIIKA <akinbuwaemmanuel6.com>
2024-04-09 19:59:37 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import Rocket from "./Components/Rocket/Rocket";
+import AboutUs from "./Components/Pages/AboutUs";
+import Products from "./Components/Pages/Products";
+import Faq from "./Components/Pages/Faq";
+import Home from "./Components/Pages/Home";
+import ContactUs from "./Components/Pages/ContactUs";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/faq" element={<Faq />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
+      </Routes>
+      
+      <Footer/>
+    
     </div>
   );
 }
